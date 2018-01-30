15d73c635085834989b81fb2a40c34561c004496
lixiaoxue <lixx@raiyee.com>
2018-01-30 22:07:58 +0800

    add *.json

--- app/Greeter.js
+++ app/Greeter.js
@@ -1,5 +1,7 @@
+const config = require('./config')
+
 module.exports = function () {
     var greet = document.createElement('div')
-    greet.textContent = "Hi there and greetings!"
+    greet.textContent = config.greetText
     return greet
 }
